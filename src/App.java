a72dfa47b4973fa9b1a01d44677ced13385e9fa3
Gaabi7 <124222168@ulife.com.br>
2025-05-20 20:41:49 -0300

    ta_indo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        pessoa p = new pessoa("Gabriela", 21, "Rua 5", "12345678909");
+
+        if (p.cpfValido()) {
+            System.out.println("CPF válido:"); 
+        } else {
+            System.out.println("Cpf Inválido");
+        }
     }
 }
--- src/pessoa.java
+++ src/pessoa.java
@@ -1,11 +1,42 @@
 public class pessoa {
     
     //Atributos 
-
     private String nome;
     private int idade;
     private String endereco;
-    private int cpf;
-    
+    private String cpf;
+
+    //Contrutor
+    public pessoa (String nome, int idade, String endereco, String cpf) {
+        this.nome = nome;
+        this.idade = idade;
+        this.endereco = endereco;
+        this.cpf = cpf;
+    }
+
+    //Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    } 
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public boolean cpfValido() {
+        String cpfNumeros = cpf.replaceAll("\\D", "");
+
+        if (cpfNumeros.length() != 11 || cpfNumeros.matches("(\\d)\\1{10}"));
+        return false;
+        
+    }
     
 }
